375fbb248b01fa347fe435cd01494922e4812e50
F0rL <kumaloveslife@163.com>
2025-03-06 14:09:03 +0800

    init

---
+++ src/components/Layout/Index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const value = ref('component')
+</script>
+
+<template>
+  <div class="container">{{ value }}</div>
+</template>
+
+<style scoped></style>
